ce651d6be1fcb000cc3cb4c1a210ca1ead46b30d
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-01 09:21:19 +0530

    behaviour of useEffect has been tested UseEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import { useEffect } from "react";
+
+const Title = () => {
+  console.log(3);
+  useEffect(() => {
+    console.log(4);
+  }, []);
+  console.log(5);
+  return <h3>Jaswant</h3>;
+};
 
 export default function App() {
+  console.log(1);
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+  console.log(2);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Title />
+      <h3> Loading and error concept </h3>
     </div>
   );
 }
