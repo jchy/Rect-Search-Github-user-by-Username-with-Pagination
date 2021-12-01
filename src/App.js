9af3d9a52e1a9c4db90eaea788c405667d36d97b
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-01 09:24:42 +0530

    isLoading has been implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import "./styles.css";
-import { useEffect } from "react";
-
-const Title = () => {
-  console.log(3);
-  useEffect(() => {
-    console.log(4);
-  }, []);
-  console.log(5);
-  return <h3>Jaswant</h3>;
-};
+import { useState, useEffect } from "react";
 
 export default function App() {
-  console.log(1);
-  useEffect(() => {
-    console.log("useEffect");
-  }, []);
-  console.log(2);
-  return (
-    <div className="App">
-      <Title />
-      <h3> Loading and error concept </h3>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {}, []);
+
+  return <div className="App">{isLoading && <h3>Loading ...</h3>}</div>;
 }
